ad155267a4d7383e2f967735ff149cd89edebd4a
刘祥 <lx_myall@163.com>
2019-12-01 19:35:12 +0800

    modified by dev

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+/*
+* 登录的路由组件
+* */
+
+export default class  Login extends Component{
+    render() {
+        return(
+            <div>Login</div>
+        )
+    }
+}
