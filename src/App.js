4d57c6f22fd19a8f0c688299719f25aab6edbf14
刘祥 <lx_myall@163.com>
2019-12-02 21:13:25 +0800

    使用ant的东西，配置了antd的相关内容，自定义主题，form静态表单的使用

--- src/App.js
+++ src/App.js
@@ -25,5 +25,6 @@ export default class App extends Component {
     }
 
 
+
 }
 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,15 +1,55 @@
 import React,{Component} from 'react'
-import "./login.css"
+import {
+    Form,
+    Icon,
+    Input,
+    Button,
+} from 'antd'; //添加ant form组件
+import "./login.less"
+import logo from './images/login-bg.png'
+
+const Item =Form.Item   //不能写在ipmpoot之前
 /*
 * 登录的路由组件
 * */
 
 export default class  Login extends Component{
+
+    // handleSubmit =(event) =>
+
     render() {
         return(
             <div className={"login"}>
-                <header className={"login-header"}></header>
-                <section className={"login-content"}></section>
+                <header className={"login-header"}>
+                    <img src={logo} alt={""}/>
+                    {/*//加载动态的值*/}
+                    <h1>哈工大实验中心</h1>
+                </header>
+                <section className={"login-content"}>
+                    <h2>用户登录</h2>
+                    {/* 分割而已*/}
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.5)' }} />}
+                                    placeholder="用户名"
+                                />
+                        </Form.Item>
+                        <Form.Item>
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.5)' }} />}
+                                    type="password"
+                                    placeholder="密码"
+                                />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                {/*    */}
+                </section>
             </div>)
 
     }
