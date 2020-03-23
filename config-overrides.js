29ffefb6bbee09d04a9e682c18a52e6b58d4b55e
seanHit <lx_myall@163.com>
2020-03-23 21:28:12 +0800

    更正了一下antd的按需打包

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,7 @@ module.exports = override(
         libraryDirectory: 'es',
         style: true,
     }),
+    //使用less-loader，相当于改less文件
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: {'@primary-color': '#1DA57A'},
